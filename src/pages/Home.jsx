76f533ab45ca8e4f3ff4e613e3c4059eebacf744
Sharu2003-27 <sharayu.borude@gmail.com>
2025-10-14 17:27:22 +0530

    Added padding to navbar

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -10,28 +10,28 @@ export default function Home() {
                         <div className='col-6 col-md-3'>
                             <div className='card p-4 bg-black border-0'>
                                 <Link to={`/productsList/home-decor`} className='text-decoration-none'>
-                                    <h4 className='text-white'>Home Decor</h4>
+                                    <h4 className='text-white' style={{height: "30px"}}>Home Decor</h4>
                                 </Link>
                             </div>
                         </div>
                         <div className='col-6 col-md-3'>
                             <div className='card p-4 bg-black border-0'>
                                 <Link to={`/productsList/living-room`} className='text-decoration-none'>
-                                    <h4 className='text-white'>Living Room</h4>
+                                    <h4 className='text-white' style={{height: "30px"}}>Living Room</h4>
                                 </Link>
                             </div>
                         </div>
                         <div className='col-6 col-md-3'>
                             <div className='card p-4 bg-black border-0'>
                                 <Link to={`/productsList/kitchen`} className='text-decoration-none'>
-                                    <h4 className='text-white'>Kitchen & Dining</h4>
+                                    <h4 className='text-white' style={{height: "30px"}}>Kitchen & Dining</h4>
                                 </Link>
                             </div>
                         </div>
                         <div className='col-6 col-md-3'>
                             <div className='card p-4 bg-black border-0'>
                                 <Link to={`/productsList/bedroom`} className='text-decoration-none'>
-                                    <h4 className='text-white'>Bedroom</h4>
+                                    <h4 className='text-white' style={{height: "30px"}}>Bedroom</h4>
                                 </Link>
                             </div>
                         </div>
@@ -47,7 +47,7 @@ export default function Home() {
 
                 <section className="p-3">
                     <div className="row">   
-                        <div className="col-md-6">
+                        <div className="col-md-6 py-3">
                             <div className="card h-100 d-flex flex-row border-0">
                                 <img src="https://i.ytimg.com/vi/KvZHL8D-RAE/maxresdefault.jpg" 
                                 alt="winter sale 1"
@@ -59,7 +59,7 @@ export default function Home() {
                                 </div>
                             </div>
                         </div>
-                        <div className="col-md-6">
+                        <div className="col-md-6 py-3">
                             <div className="card h-100 d-flex flex-row border-0">
                                 <img src="https://www.coversandall.com/blog/wp-content/uploads/2021/11/Tips-to-Winterize-and-Store-Your-Outdoor-Furniture.png"
                                  alt="" 
